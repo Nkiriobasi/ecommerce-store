00aa8bd5e163854541bcda3c72c27de2a35275f2
Nkiriobasi <obasinkiri@gmail.com>
2023-11-17 18:09:34 +0100

    modified UI

---
+++ app/layouts/blogs/findBy/findBy.tsx
@@ -0,0 +1,20 @@
+
+
+const FindBy = () => {
+  return (
+    <div className='filter-card mb-3'>
+      <h3 className="filter-title">Find By Categories</h3>
+
+      <div>
+        <ul className='ps-0 mb-0'>
+          <li>Home</li>
+          <li>Our Store</li>
+          <li>Blogs</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default FindBy
