39586ebc74dea0156c335df4bf3278b4ab7fd94a
Nkiriobasi <obasinkiri@gmail.com>
2023-11-17 18:10:16 +0100

    modified UI

---
+++ app/layouts/contact/contactUs/contactUs.tsx
@@ -0,0 +1,89 @@
+import { IoHome } from "react-icons/io5";
+import { FaPhone, FaCalendar } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+
+const ContactUs = () => {
+  return (
+    <div className="contact-inner-wrapper d-flex justify-content-between">
+        <div className="">
+            <h3 className="contact-title mb-4">Contact</h3>
+
+            <form className='d-flex flex-column gap-15'>
+                <div className='d-inline-flex align-items-center'>
+                    <input 
+                        type="text" 
+                        placeholder='Name'
+                        name='fullName'
+                        id='fullName'
+                        className='form-control' 
+                    />
+                </div>
+                <div className='d-inline-flex align-items-center'>
+                    <input 
+                        type="email" 
+                        placeholder='Email'
+                        name='emailAddress'
+                        id='emailAddress'
+                        className='form-control' 
+                    />
+                </div>
+                <div className='d-inline-flex align-items-center'>
+                    <input 
+                        type="tel" 
+                        placeholder='Phone number'
+                        name='phoneNumber'
+                        id='phoneNumber'
+                        className='form-control' 
+                    />
+                </div>
+                <div className='d-inline-flex align-items-center'>
+                    <textarea 
+                        name="comment" 
+                        placeholder='Comment'
+                        className='w-100 form-control' 
+                        rows={5}
+                        id="comment"
+                    ></textarea>
+                </div>
+                <div className='d-inline-flex align-items-center'>
+                    <button className='button border-0'>Submit</button>
+                </div>
+            </form>
+        </div>
+
+        <div className="">
+            <h3 className="contact-title mb-4">Get in touch with us</h3>
+
+            <div className="">
+                <ul className="ps-0">
+                    <li className="mb-4 d-flex gap-15 align-items-center">
+                        <IoHome className="fs-5" />
+                        <address className="mb-0">No. 1234 Aje Lagos, Nigeria.</address>
+                    </li>
+                    <li className="mb-4 d-flex gap-15 align-items-center">
+                        <FaPhone className="fs-5" />
+                        <a className="" href="tel:+234 7037648977">
+                            +234 7037648977
+                        </a>
+                    </li>
+                    <li className="mb-4 d-flex gap-15 align-items-center">
+                        <MdEmail className="fs-5" />
+                        <a 
+                            href="mailto:exampleemail@gmail.com" 
+                            className="d-block mb-0"
+                        >
+                            exampleemail@gmail.com
+                        </a>
+                    </li>
+                    <li className="mb-4 d-flex gap-15 align-items-center">
+                        <FaCalendar className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10 AM - 8 PM</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ContactUs
---
+++ app/layouts/contact/map/map.tsx
@@ -0,0 +1,18 @@
+
+
+const Map = () => {
+  return (
+    <>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15898.968394349931!2d8.3563798!3d4.9824641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1067870bafffffff%3A0x914a7b0dd9cd49ef!2sFederal%20Government%20Girls%20College!5e0!3m2!1sen!2sng!4v1700231707248!5m2!1sen!2sng" 
+            width="600" 
+            height="450" 
+            className="border-0 w-100" 
+            allowFullScreen
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+    </>
+  )
+}
+
+export default Map
